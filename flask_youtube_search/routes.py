733c297134380e1f85638e5487f19df48183683f
Serdar Kuyuk <serdarkuyuk@gmail.com>
2020-02-07 22:37:19 -0500

    deleting unnecssary files

--- flask_youtube_search/routes.py
+++ flask_youtube_search/routes.py
@@ -16,6 +16,8 @@ def index():
     videos = []
 
     if request.method == 'POST':
+
+        #define search parameters
         search_params = {
             'key' : current_app.config['YOUTUBE_API_KEY'],
             'q' : request.form.get('query'),
@@ -25,49 +27,59 @@ def index():
             'videoCaption' : 'closedCaption'
         }
 
+        #get search results
         r = requests.get(search_url, params=search_params)
 
+        #parse the request and get items
         results = r.json()['items']
 
+        #get list of video ID's which will be used to get captions
         video_ids = []
         for result in results:
             video_ids.append(result['id']['videoId'])
 
+        #if "I am hungry" button submitted, first video will be directed 
         if request.form.get('submit') == 'lucky':
-            print(video_ids[0])
             return redirect(f'https://www.youtube.com/watch?v={ video_ids[0] }')
-            
+        
+        #define video search parameters   
         video_params = {
             'key' : current_app.config['YOUTUBE_API_KEY'],
             'id' : ','.join(video_ids),
             'part' : 'snippet,contentDetails',
-            'maxResults' : 2
+            'maxResults' : 9
         }
-#result['snippet']['description']
+
+        #get video results with video parameters
         r = requests.get(video_url, params=video_params)
-        results = r.json()['items']
+        results = r.json()['items'] #parse the request and get items
+
+        #for each video 
         for result in results:
+
+            #video identifiers            
             video_data = {
                 'id' : result['id'],
                 'url' : f'https://www.youtube.com/watch?v={ result["id"] }',
                 'thumbnail' : result['snippet']['thumbnails']['high']['url'],
                 'duration' : int(parse_duration(result['contentDetails']['duration']).total_seconds() // 60),
-                'title' : result['snippet']['title'],
-                'description' : result['snippet']['description'],
-                'description2' : 'som'
-                #'language' : result['snippet'][language
+                'title' : result['snippet']['title']
             }
-            #ruledbase_parsed_text = parsing_hsk_v3.hsk(video_data['description'])
-            #print(type(result["id"]))
 
-            results1= model_results.extract_ingredients(str(result["id"]))
-            if len(results1) > 15:
-                video_data['description'] = results1[:15] #''.join(results)
-                video_data['description2'] = results1[15:31]
+            #extract ingredienst 
+            list_ingredients = model_results.extract_ingredients(str(result["id"]))
+
+            #make list to publish in website
+            if len(list_ingredients) > 15:
+                #if number of ingredients 15, show in the other colomn
+                video_data['ingredients_p1'] = list_ingredients[:15]   
+                video_data['ingredients_p2'] = list_ingredients[15:31]
             else:
-                video_data['description'] = results1[:15]
-                video_data['description2'] = []
+                video_data['ingredients_p1'] = list_ingredients[:15]
+                video_data['ingredients_p2'] = []
+
             videos.append(video_data)
+
     return render_template('index.html', videos=videos)
 
 
